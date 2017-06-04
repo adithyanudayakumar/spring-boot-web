84203907cc1af865cfc495e66af0cce0d762cfc4
adithyanudayakumar <adithyan.udayakumar@gmail.com>
2017-06-04 17:14:28 -0400

    first commit

---
+++ src/main/java/com/adi/controller/SpringRestController.java
@@ -0,0 +1,24 @@
+package com.adi.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/rest")
+public class SpringRestController {
+
+	// inject via application.properties
+	@Value("${welcome.message:test}")
+	private String message = "Hello Adi from rest controller";
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "welcome";
+	}
+
+}
